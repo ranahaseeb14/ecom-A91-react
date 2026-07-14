588a0cefebef61a99c0216f70109a500f855661c
Rana-Home <haseebofficial14@gmail.com>
2026-07-15 00:59:06 +0500

    removed import.env

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,5 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import "bootstrap/dist/css/bootstrap.min.css"
 import App from './App'
-import.meta.env.MODE
-import.meta.env.DEV
-import.meta.env.PROD
-import.meta.env.VITE_SOME_VALUE
 
 createRoot(document.getElementById('root')).render(<App />)
